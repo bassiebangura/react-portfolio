5a4fa6ef0c7702541f1f04902c7ec6533dc755e1
BassieBangura <devbassiebangura@gmail.com>
2022-11-05 17:31:49 -0500

    Update cursor for nav items

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -12,14 +12,14 @@ export default function Navigation(props) {
       <nav className="w-full md:w-auto">
         <ul className="ml-10 hidden space-x-8 md:flex">
           {navItems.map((item) => (
-            <li className={`text-base font-medium text-white hover:text-gray-300 ${activeTab === item.title && 'border-b-2'}`} key={item.title}>
+            <li className={`cursor-pointer text-base font-medium text-white hover:text-gray-300 ${activeTab === item.title && 'border-b-2'}`} key={item.title}>
               <span onClick={() => setCurrentTitle(item.title)}>{item.title}</span>
             </li>
           ))}
         </ul>
         <ul className="flex w-full justify-center space-x-6 py-4 md:hidden">
           {navItems.map((item) => (
-            <li className={`text-base font-medium text-white hover:text-gray-300 ${activeTab === item.title && 'border-b-2'}`} key={item.title}>
+            <li className={`cursor-pointer text-base font-medium text-white hover:text-gray-300 ${activeTab === item.title && 'border-b-2'}`} key={item.title}>
               <span onClick={() => setCurrentTitle(item.title)}>{item.title}</span>
             </li>
           ))}
